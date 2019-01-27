4f3e19c0449cdc38f6b85fc5c30a9bfb1cbe9cc5
root <root@Nicks-pc.localdomain>
2019-01-26 21:34:03 -0500

    refactor and include a01q4 solutions

--- a01.py
+++ a01.py
@@ -1,40 +1,74 @@
-from methods import EulersMethod, ThetaMethod
-import numpy as np
-from math import exp
 from functools import partial
+from math import exp
+from methods import EulersMethod, ThetaMethod
+from numpy import array as vec
 
-def f(x):
-    ''' ODE function for y'=f
-    Q3-Q4) y'' + 10y' + 25y = 0
+def Q3():
+    ''' for 3 h-values, plot the theta/euler method vs solution
+    and find max error
     '''
-    return np.array([
-        x[1],
-        -10*(x[1] + 2.5 * x[0])
-    ])
+    # ODE function for y'=f ; y'' + 10y' + 25y = 0
+    f = lambda x: vec([ x[1], -10*(x[1] + 2.5 * x[0]) ])
+    # Exact solution to y'=f ; y'' + 10y' + 25y = 0
+    y = lambda t: exp(-5*t) * (5*t + 1)
 
-def y(t):
-    ''' Exact solution to y'=f
-    Q3-Q4) y'' + 10y' + 25y = 0
+    e_file = open('a01/q3_maxerror.txt', 'w')
+    for h in (0.5, 0.05, 0.005):
+        sol = EulersMethod(
+            f=f,
+            y0=vec([1,0]),
+            h=h,
+            t0=0,
+            tf=1
+        )
+        sol.plot_first_coord(
+            title='Actual vs Approximated y(t); ' \
+                  'Euler\'s Method with h={0}'.format(h),
+            filename='a01/q3c_h{0}.png'.format(str(h).replace('.', '')),
+            y=y
+        )
+        e_file.write('EulersMethod ; h={0}\n{1}\n'.format(h, sol.maxError(y)))
+    
+        sol = ThetaMethod(
+            theta=0.5,
+            f=f,
+            y0=vec([1,0]),
+            h=h,
+            t0=0,
+            tf=1
+        )
+        sol.plot_first_coord(
+            title='Actual vs Approximated y(t); '
+                  'Theta Method with h={0}'.format(h),
+            filename='a01/q3e_h{0}.png'.format(str(h).replace('.', '')),
+            y=y
+        )
+        e_file.write('ThetaMethod ; h={0}\n{1}\n'.format(h, sol.maxError(y)))
+
+    e_file.close()
+
+def Q4():
+    ''' for 2 rho values, plot the euler method approximations
+    for Lorenz equations
     '''
-    return exp(-5*t) * (5*t + 1)
-
-def write_max_error(e_file, method, error):
-    e_file.write('{0}\n{1}\n'.format(method, error))
-
-def plot_solution(h, title, Method, q, e_file):
-    sol = Method(f, [1,0], h, 0, 1)
-    _h = str(h).replace('.','')
-    sol.plotWithSolution(y, title.format(h)).savefig('a01/q{0}_h{1}.png'.format(q, _h))
-    write_max_error(e_file, '{0} ; h={1}'.format(type(sol).__name__, h), sol.maxError(y))
+    # ODE function for y'=f ; Lorenz equations
+    _f = lambda rho, x, y, z: vec([ 10*(y-x), x*(rho - z) - y, x*y - (8/3)*z ])
+    
+    for rho in (14, 28):
+        f = lambda x: _f(rho, x[0], x[1], x[2])
+        EulersMethod(
+            f=f,
+            y0=vec([1,0,0]),
+            h=0.002,
+            t0=0,
+            tf=50
+        ).plot_first_coord(
+            title='Approximated x(t); ' \
+                  'Euler\'s Method with rho={0}'.format(rho),
+            filename='a01/q4{0}.png'.format(rho)
+        )
 
 if __name__ == "__main__":
-    e_file = open('a01/q3q4_maxerror.txt', 'w')
-    for h in (0.5, 0.05, 0.005):
-        title = 'Actual vs Approximated y(t); Euler\'s Method with h={0}'
-        plot_solution(h, title, EulersMethod, '3', e_file)
-    
-        title = 'Actual vs Approximated y(t); Theta Method with h={0}'
-        Method = partial(ThetaMethod, 0.5)  # theta = 0.5
-        plot_solution(h, title, Method, '4', e_file)
-    e_file.close()
-    
+    Q3()
+    Q4()
+
--- methods.py
+++ methods.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from collections import defaultdict
 from math import floor
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy import array as vec
 from sympy import linsolve, symbols
 
 
@@ -25,21 +27,34 @@ class NumericalMethod(object):
         t_f num ; final time
         '''
         self.N = int(((tf-t0)/h)//1)
-        self.t = [t0 + n*h for n in range(self.N+1)] # N+1 to accommodate t0, tf
-        self.y = [np.array(y0)]
+        self.t = [t0 + n*h for n in range(self.N+1)] # N+1 to include tf
+        self.y = [y0]
         self.f = [f(y0)]
         for n in range(self.N):
-            self.step(h, n, f)
+            try:
+                self.step(h, n, f)
+            except:
+                f_len = len(self.f)
+                if f_len < len(self.y):  # rollback change
+                    self.y = self.y[:f_len]
+                self.t = self.t[:f_len]
+                break
 
-    def plotWithSolution(self, y, title):
-        t = np.linspace(self.t[0], self.t[-1], 500)
-        y = [y(_t) for _t in t]
-
+    def plot_first_coord(self, title, filename, y=None):
+        ''' plot approximated first coordinate of the solution
+        against provided function y(t) if provided
+        '''
         fig, ax = plt.subplots()
         ax.plot(self.t, [pt[0] for pt in self.y], 'r')
-        ax.plot(t, y, 'b')
+
+        if y is not None:
+            t = np.linspace(float(self.t[0]), float(self.t[-1]), 500)
+            y = [y(_t) for _t in t]
+            ax.plot(t, y, 'b')
+            ax.legend(['Approximate', 'Actual'])
+
         ax.set(xlabel='t', ylabel='y(t)', title=title)
-        ax.legend(['Approximate', 'Actual'])
+        fig.savefig(filename)
         return fig
 
     def maxError(self, y):
@@ -55,27 +70,33 @@ class EulersMethod(NumericalMethod):
         ''' Calculate the (n+1)th step and append it onto self.y, self.f
         '''
         self.y.append(self.y[n] + h*self.f[n])
-        self.f.append(f(self.y[-1]))
+        try:
+            self.f.append(f(self.y[-1]))
+        except:
+            print([h, n, self.y[-1]])
+            print(self.y[:5])
+            raise
 
 
 class ThetaMethod(NumericalMethod):
     ''' Implementation of Theta Method
     '''
-    def __init__(self, theta, *args):
+    def __init__(self, theta, f, y0, h, t0, tf):
         self.theta = theta
-        super(ThetaMethod, self).__init__(*args)
+        super(ThetaMethod, self).__init__(f, y0, h, t0, tf)
 
     def step(self, h, n, f):
         ''' Calculate the (n+1)th step and append it onto self.y, self.f
         '''
         y1, y2 = symbols('y1, y2')
         y = np.array([y1, y2])
-        step = np.array(next(iter(linsolve([
-            y1 - self.y[n][0] - h*self.theta*self.f[n][0] - h*(1-self.theta)*f(y)[0],
-            y2 - self.y[n][1] - h*self.theta*self.f[n][1] - h*(1-self.theta)*f(y)[1]
+        theta = self.theta
+        step = vec(next(iter(linsolve([
+            y1 - self.y[n][0] - h*theta*self.f[n][0] - h*(1-theta)*f(y)[0],
+            y2 - self.y[n][1] - h*theta*self.f[n][1] - h*(1-theta)*f(y)[1]
             ],
             (y1, y2)
         ))))
-        self.y.append(step)
-        self.f.append(f(step))
+        self.y.append(vec(step))
+        self.f.append(vec(f(step)))
 
